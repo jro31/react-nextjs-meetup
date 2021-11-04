bce9de543c4ed10a8abecc4bdb83ec40845503b3
Jethro Williams <jethrowilliams@outlook.com>
2021-11-04 16:45:28 +0000

    Populate the 'MeetupDetails' component (with the new 'MeetupDetail' component)
    Nothing very exciting here

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Englischer_Garten_M%C3%BCnchen.jpg/1280px-Englischer_Garten_M%C3%BCnchen.jpg'
+      title='Some hardcoded title'
+      address='Some hardcoded address'
+      description='Some hardcoded description'
+    />
+  );
+};
+
+export default MeetupDetails;
