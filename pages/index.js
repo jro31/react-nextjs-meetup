95752d8b6586b7274b16b9ed6e2e94c67adb9edb
Jethro Williams <jethrowilliams@outlook.com>
2021-11-04 15:56:01 +0000

    Add some dummy data, and start connecting the React components that were part of the initial project
    Nothing exciting here

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Layout from '../components/layout/Layout';
+import MeetUpList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'My First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG/1280px-Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG',
+    address: 'My First Meetup address',
+    description: 'This is the first meetup description',
+  },
+  {
+    id: 'm2',
+    title: 'My Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Englischer_Garten_M%C3%BCnchen.jpg/1280px-Englischer_Garten_M%C3%BCnchen.jpg',
+    address: 'My Second Meetup address',
+    description: 'This is the second meetup description',
+  },
+];
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <MeetUpList meetups={DUMMY_MEETUPS} />
+    </Layout>
+  );
+};
+
+export default HomePage;
